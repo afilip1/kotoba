90612b587b4aa0fdcc80d848566960eaee0db683
Ristarg <a.filippov@protonmail.com>
2018-07-02 23:46:37 +0700

    Refactored all sorts of things in lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,14 @@
 use std;
 
-#[derive(Debug)]
+macro_rules! hashmap {
+    ($($key:expr => $value:expr),*) => ({
+        let mut tmp = std::collections::HashMap::new();
+        $(tmp.insert($key, $value);)*
+        tmp
+    });
+}
+
+#[derive(Debug, Clone)]
 pub enum TokenKind {
     Number(f64),
     Boolean(bool),
@@ -8,11 +16,12 @@ pub enum TokenKind {
     StringLiteral(String),
     Nil,
     Equal,
-    NotEqual,
+    EqualEqual,
+    BangEqual,
     Greater,
-    GreaterOrEqual,
+    GreaterEqual,
     Less,
-    LessOrEqual,
+    LessEqual,
     Plus,
     Minus,
     Star,
@@ -22,7 +31,7 @@ pub enum TokenKind {
     CloseParen,
 }
 
-pub  struct Lexer<'a> {
+pub struct Lexer<'a> {
     source: &'a [u8],
     index: usize,
 }
@@ -38,11 +47,17 @@ impl<'a> Lexer<'a> {
     pub fn tokenize(&mut self) -> Vec<TokenKind> {
         let mut tokens = vec![];
 
-        while let Some(c) = self.source.get(self.index) {
+        let lookahead_map = hashmap! {
+            b'=' => (TokenKind::EqualEqual, TokenKind::Equal),
+            b'!' => (TokenKind::BangEqual, TokenKind::Bang),
+            b'>' => (TokenKind::GreaterEqual, TokenKind::Greater),
+            b'<' => (TokenKind::LessEqual, TokenKind::Less)
+        };
+
+        while let Some(c) = self.current() {
             match c {
-                b' ' | b'\t' | b'\n' => self.index += 1,
-                b'0'...b'9' => tokens.push(TokenKind::Number(self.number())),
-                b'a'...b'z' | b'A'...b'Z' | b'_' => tokens.push(match self.identifier() {
+                b'0'...b'9' => tokens.push(TokenKind::Number(self.consume_number())),
+                b'a'...b'z' | b'A'...b'Z' | b'_' => tokens.push(match self.consume_identifier() {
                     "true" => TokenKind::Boolean(true),
                     "false" => TokenKind::Boolean(false),
                     "nil" => TokenKind::Nil,
@@ -50,78 +65,40 @@ impl<'a> Lexer<'a> {
                 }),
                 b'"' => {
                     self.index += 1;
-                    let string_contents = self.string().to_owned();
-                    if let Some(b'"') = self.source.get(self.index) {
+                    let string_contents = self.consume_string();
+                    if let Some(b'"') = self.current() {
+                        tokens.push(TokenKind::StringLiteral(string_contents.to_owned()));
                         self.index += 1;
-                        tokens.push(TokenKind::StringLiteral(string_contents));
                     } else {
                         println!("Unclosed string literal");
                         std::process::exit(1);
                     }
                 }
-                b'=' => if let Some(b'=') = self.source.get(self.index + 1) {
-                    self.index += 2;
-                    tokens.push(TokenKind::Equal);
-                } else {
-                    println!("Assignment is not supported at this time");
-                    std::process::exit(2);
-                },
-                b'!' => match self.source.get(self.index + 1) {
+                c @ b'=' | c @ b'!' | c @ b'>' | c @ b'<' => match self.peek() {
                     Some(b'=') => {
+                        tokens.push(lookahead_map[c].0.clone());
                         self.index += 2;
-                        tokens.push(TokenKind::NotEqual);
                     }
-                    _ => {
+                    Some(_) => {
+                        tokens.push(lookahead_map[c].1.clone());
                         self.index += 1;
-                        tokens.push(TokenKind::Bang);
                     }
+                    _ => self.index += 1,
                 },
-                b'>' => match self.source.get(self.index + 1) {
-                    Some(b'=') => {
-                        self.index += 2;
-                        tokens.push(TokenKind::GreaterOrEqual);
+                size_1 => {
+                    match size_1 {
+                        b' ' | b'\t' | b'\n' => { /* skip whitespace */ }
+                        b'+' => tokens.push(TokenKind::Plus),
+                        b'-' => tokens.push(TokenKind::Minus),
+                        b'*' => tokens.push(TokenKind::Star),
+                        b'/' => tokens.push(TokenKind::Slash),
+                        b'(' => tokens.push(TokenKind::OpenParen),
+                        b')' => tokens.push(TokenKind::CloseParen),
+                        other => println!(
+                            "Unrecognized byte: '{}' (0x{:x}), skipping...",
+                            *other as char, *other
+                        ),
                     }
-                    _ => {
-                        self.index += 1;
-                        tokens.push(TokenKind::Greater);
-                    }
-                },
-                b'<' => match self.source.get(self.index + 1) {
-                    Some(b'=') => {
-                        self.index += 2;
-                        tokens.push(TokenKind::LessOrEqual);
-                    }
-                    _ => {
-                        self.index += 1;
-                        tokens.push(TokenKind::Less);
-                    }
-                },
-                b'+' => {
-                    tokens.push(TokenKind::Plus);
-                    self.index += 1;
-                }
-                b'-' => {
-                    tokens.push(TokenKind::Minus);
-                    self.index += 1;
-                }
-                b'*' => {
-                    tokens.push(TokenKind::Star);
-                    self.index += 1;
-                }
-                b'/' => {
-                    tokens.push(TokenKind::Slash);
-                    self.index += 1;
-                }
-                b'(' => {
-                    tokens.push(TokenKind::OpenParen);
-                    self.index += 1;
-                }
-                b')' => {
-                    tokens.push(TokenKind::CloseParen);
-                    self.index += 1;
-                }
-                other => {
-                    println!("Unrecognized byte: '{}' (0x{:x})", *other as char, *other);
                     self.index += 1;
                 }
             }
@@ -129,9 +106,17 @@ impl<'a> Lexer<'a> {
         tokens
     }
 
-    fn string(&mut self) -> &str {
+    fn current(&self) -> Option<&u8> {
+        self.source.get(self.index)
+    }
+
+    fn peek(&self) -> Option<&u8> {
+        self.source.get(self.index + 1)
+    }
+
+    fn consume_string(&mut self) -> &'a str {
         let start = self.index;
-        while let Some(c) = self.source.get(self.index) {
+        while let Some(c) = self.current() {
             if *c == b'"' {
                 break;
             }
@@ -141,9 +126,9 @@ impl<'a> Lexer<'a> {
         std::str::from_utf8(&self.source[start..end]).unwrap()
     }
 
-    fn identifier(&mut self) -> &str {
+    fn consume_identifier(&mut self) -> &str {
         let start = self.index;
-        while let Some(c) = self.source.get(self.index) {
+        while let Some(c) = self.current() {
             if !c.is_ascii_alphanumeric() && *c != b'_' {
                 break;
             }
@@ -153,15 +138,15 @@ impl<'a> Lexer<'a> {
         std::str::from_utf8(&self.source[start..end]).unwrap()
     }
 
-    fn number(&mut self) -> f64 {
+    fn consume_number(&mut self) -> f64 {
         let start = self.index;
-        while let Some(b'0'...b'9') = self.source.get(self.index) {
+        while let Some(b'0'...b'9') = self.current() {
             self.index += 1;
         }
-        if let Some(b'.') = self.source.get(self.index) {
-            if let Some(b'0'...b'9') = self.source.get(self.index + 1) {
+        if let Some(b'.') = self.current() {
+            if let Some(b'0'...b'9') = self.peek() {
                 self.index += 2;
-                while let Some(b'0'...b'9') = self.source.get(self.index) {
+                while let Some(b'0'...b'9') = self.current() {
                     self.index += 1;
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+#![feature(nll)]
 mod lexer;
 use lexer::*;
 
 fn main() {
     // let source = "123.434 true false nil \"test \nstring\" + - * / == != >= <= > < ! ()";
-    let source = "(1 + 345.67) / some_var >= function(arg)";
+    let source = "(1 + 345.67) / some_var >= function(\"some string\")";
     let mut lexer = Lexer::new(source);
 
     let tokens = lexer.tokenize();
