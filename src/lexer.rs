0ef3cffb379b4fc40019c1a392e464b24f48a7b4
Ristarg <a.filippov@protonmail.com>
2018-07-06 01:45:56 +0700

    Initial token position implementation (God have mercy)

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,6 +6,18 @@ macro_rules! hashmap {
     });
 }
 
+#[derive(Debug)]
+pub struct Position {
+    pub line: usize,
+    pub character: usize,
+}
+
+#[derive(Debug)]
+pub struct Token<'a> {
+    pub kind: TokenKind<'a>,
+    pub position: Position,
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum TokenKind<'a> {
     Number(f64),
@@ -32,6 +44,8 @@ pub enum TokenKind<'a> {
 pub struct Lexer<'a> {
     source: &'a [u8],
     index: usize,
+    cur_line: usize,
+    cur_char: usize,
 }
 
 impl Lexer<'a> {
@@ -39,10 +53,12 @@ impl Lexer<'a> {
         Lexer {
             source: source.as_bytes(),
             index: 0,
+            cur_line: 0,
+            cur_char: 0,
         }
     }
 
-    pub fn tokenize(&mut self) -> Vec<TokenKind> {
+    pub fn tokenize(&mut self) -> Vec<Token> {
         let mut tokens = vec![];
 
         let lookahead_map = hashmap! {
@@ -54,19 +70,41 @@ impl Lexer<'a> {
 
         while let Some(c) = self.current() {
             match c {
-                b'0'...b'9' => tokens.push(TokenKind::Number(self.consume_number())),
-                b'a'...b'z' | b'A'...b'Z' | b'_' => tokens.push(match self.consume_identifier() {
-                    "true" => TokenKind::Boolean(true),
-                    "false" => TokenKind::Boolean(false),
-                    "nil" => TokenKind::Nil,
-                    other => TokenKind::Identifier(other),
+                b'0'...b'9' => tokens.push(Token {
+                    position: Position {
+                        line: self.cur_line,
+                        character: self.cur_char,
+                    },
+                    kind: TokenKind::Number(self.consume_number()),
+                }),
+                b'a'...b'z' | b'A'...b'Z' | b'_' => tokens.push(Token {
+                    position: Position {
+                        line: self.cur_line,
+                        character: self.cur_char,
+                    },
+                    kind: match self.consume_identifier() {
+                        "true" => TokenKind::Boolean(true),
+                        "false" => TokenKind::Boolean(false),
+                        "nil" => TokenKind::Nil,
+                        other => TokenKind::Identifier(other),
+                    },
                 }),
                 b'"' => {
+                    let line = self.cur_line;
+                    let ch = self.cur_char;
                     self.index += 1;
+                    self.cur_char += 1;
                     let string_contents = self.consume_string();
                     if let Some(b'"') = self.current() {
-                        tokens.push(TokenKind::StringLiteral(string_contents));
+                        tokens.push(Token {
+                            kind: TokenKind::StringLiteral(string_contents),
+                            position: Position {
+                                line: line,
+                                character: ch,
+                            },
+                        });
                         self.index += 1;
+                        self.cur_char += 1;
                     } else {
                         println!("Unclosed string literal");
                         std::process::exit(1);
@@ -74,30 +112,88 @@ impl Lexer<'a> {
                 }
                 c @ b'=' | c @ b'!' | c @ b'>' | c @ b'<' => match self.peek() {
                     Some(b'=') => {
-                        tokens.push(lookahead_map[c].0);
+                        tokens.push(Token {
+                            kind: lookahead_map[c].0,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        });
                         self.index += 2;
+                        self.cur_char += 2;
                     }
                     Some(_) => {
-                        tokens.push(lookahead_map[c].1);
+                        tokens.push(Token {
+                            kind: lookahead_map[c].1,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        });
                         self.index += 1;
+                        self.cur_char += 1;
                     }
-                    _ => self.index += 1,
+                    _ => {
+                        self.index += 1;
+                        self.cur_char += 1;
+                    }
                 },
                 size_1 => {
                     match size_1 {
-                        b' ' | b'\t' | b'\n' => { /* skip whitespace */ }
-                        b'+' => tokens.push(TokenKind::Plus),
-                        b'-' => tokens.push(TokenKind::Minus),
-                        b'*' => tokens.push(TokenKind::Star),
-                        b'/' => tokens.push(TokenKind::Slash),
-                        b'(' => tokens.push(TokenKind::OpenParen),
-                        b')' => tokens.push(TokenKind::CloseParen),
+                        b' ' | b'\t' => { /* skip whitespace */ }
+                        b'\n' => {
+                            self.cur_line += 1;
+                            self.cur_char = 0;
+                        }
+                        b'+' => tokens.push(Token {
+                            kind: TokenKind::Plus,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
+                        b'-' => tokens.push(Token {
+                            kind: TokenKind::Minus,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
+                        b'*' => tokens.push(Token {
+                            kind: TokenKind::Star,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
+                        b'/' => tokens.push(Token {
+                            kind: TokenKind::Slash,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
+                        b'(' => tokens.push(Token {
+                            kind: TokenKind::OpenParen,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
+                        b')' => tokens.push(Token {
+                            kind: TokenKind::CloseParen,
+                            position: Position {
+                                line: self.cur_line,
+                                character: self.cur_char,
+                            },
+                        }),
                         other => println!(
                             "Unrecognized byte: '{}' (0x{:x}), skipping...",
                             *other as char, *other
                         ),
                     }
                     self.index += 1;
+                    self.cur_char += 1;
                 }
             }
         }
@@ -119,6 +215,7 @@ impl Lexer<'a> {
                 break;
             }
             self.index += 1;
+            self.cur_char += 1;
         }
         let end = self.index;
         std::str::from_utf8(&self.source[start..end]).unwrap()
@@ -131,6 +228,7 @@ impl Lexer<'a> {
                 break;
             }
             self.index += 1;
+            self.cur_char += 1;
         }
         let end = self.index;
         std::str::from_utf8(&self.source[start..end]).unwrap()
@@ -140,12 +238,15 @@ impl Lexer<'a> {
         let start = self.index;
         while let Some(b'0'...b'9') = self.current() {
             self.index += 1;
+            self.cur_char += 1;
         }
         if let Some(b'.') = self.current() {
             if let Some(b'0'...b'9') = self.peek() {
                 self.index += 2;
+                self.cur_char += 2;
                 while let Some(b'0'...b'9') = self.current() {
                     self.index += 1;
+                    self.cur_char += 1;
                 }
             }
         }
