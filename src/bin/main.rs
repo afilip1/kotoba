df23147788ea7b6960e03574fd417c9aa7d19542
Anton Filippov <a.filippov@protonmail.com>
2018-07-16 14:36:22 +0700

    Disabled debug printouts

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,11 +6,14 @@ fn main() {
     loop {
         print!(">>> ");
         std::io::stdout().flush().unwrap();
+
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
+
         let ast = Parser::new(&input).parse();
 
-        println!("Parsed AST:\n{:#?}\n", ast);
-        println!("Expression evaluates to:\n{:?}", eval(&ast));
+        // println!("Parsed AST:\n{:#?}\n", ast);
+        // println!("Expression evaluates to:\n{:?}", eval(&ast));
+        println!("{}", eval(&ast));
     }
 }
