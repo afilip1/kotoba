44794482aaccc539529a1159e21765cb89312775
Anton Filippov <a.filippov@protonmail.com>
2018-07-22 15:22:44 +0700

    Added file evaluation mode

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,20 @@
 use kotoba::{eval::Environment, parser::Parser};
-use std::io::{stdin, stdout, Write};
+use std::{
+    fs::File,
+    io::{stdin, stdout, Read, Write},
+};
 
 fn main() {
-    let mut env = Environment::new();
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() == 1 {
+        start_repl()
+    } else {
+        interpret_file(&args[1])
+    }
+}
 
+fn start_repl() -> ! {
+    let mut env = Environment::new();
     loop {
         print!("::<> ");
         stdout().flush().unwrap();
@@ -15,4 +26,14 @@ fn main() {
 
         println!("{}", env.eval(&ast));
     }
+}
+
+fn interpret_file(path: &str) {
+    let mut source = String::new();
+    File::open(path)
+        .expect("couldn't open file")
+        .read_to_string(&mut source)
+        .expect("couldn't read file");
+
+    println!("{}", Environment::new().eval(&Parser::new(&source).parse()));
 }
