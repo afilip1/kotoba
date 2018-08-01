64f28d3164871ce753a60e3f5733333d35b29a1c
Anton Filippov <a.filippov@protonmail.com>
2018-08-01 12:55:25 +0700

    Parser for the new syntax (sorry for dump commit)

--- src/parser.rs
+++ src/parser.rs
@@ -5,23 +5,33 @@ type Result = std::result::Result<AstNode, Error>;
 #[derive(Debug)]
 enum Error {
     UnclosedGrouping(Token),
-    UnclosedBlock(Token),
     UnexpectedToken(Token),
     UnexpectedEof,
-    AssignmentMissingEqual(Token),
-    AssignmentMissingIdentifier(Token),
-    IfMissingThenClause(Token),
+    MissingColon(Token),
     MissingSemicolon(Token),
+    FnCallMissingCloseParen(Token),
+    MissingIdentifier(Token),
+    MissingParen(Token),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum AstNode {
+    Program(Vec<AstNode>),
+
     Number(f64),
     Boolean(bool),
     StringLiteral(String),
-    Grouping(Box<AstNode>),
-    Block(Box<AstNode>),
     Identifier(String),
+    Nil,
+
+    Grouping(Box<AstNode>),
+
+    FnCall {
+        identifier: String,
+        args: Vec<AstNode>,
+    },
+    RetStmt(Box<AstNode>),
+
     UnaryExpr {
         operator: Op,
         operand: Box<AstNode>,
@@ -31,17 +41,25 @@ pub enum AstNode {
         lhs: Box<AstNode>,
         rhs: Box<AstNode>,
     },
-    Program(Vec<AstNode>),
+
     Assignment {
         identifier: String,
         operand: Box<AstNode>,
     },
-    IfExpr {
-        check: Box<AstNode>,
-        then: Box<AstNode>,
-        otherwise: Option<Box<AstNode>>,
+    IfStmt {
+        condition: Box<AstNode>,
+        then_body: Box<AstNode>,
+        else_body: Option<Box<AstNode>>,
     },
-    Nil,
+    WhileStmt {
+        condition: Box<AstNode>,
+        body: Box<AstNode>,
+    },
+    FnStmt {
+        identifier: String,
+        params: Vec<String>,
+        body: Box<AstNode>,
+    },
 }
 
 #[derive(Debug, PartialEq)]
@@ -51,6 +69,7 @@ pub enum Op {
     Slash,
     Plus,
     Minus,
+    Percent,
     Greater,
     GreaterEqual,
     Less,
@@ -69,6 +88,7 @@ impl From<&TokenKind> for Op {
             TokenKind::Slash => Op::Slash,
             TokenKind::Plus => Op::Plus,
             TokenKind::Minus => Op::Minus,
+            TokenKind::Percent => Op::Percent,
             TokenKind::Greater => Op::Greater,
             TokenKind::GreaterEqual => Op::GreaterEqual,
             TokenKind::Less => Op::Less,
@@ -94,79 +114,134 @@ impl Parser<'source> {
     }
 
     pub fn parse(&mut self) -> AstNode {
-        match self.parse_program() {
-            Ok(p) => p,
-            Err(err) => {
-                println!("syntax error: {:#?}", err);
-                AstNode::Nil
-            }
-        }
+        self.parse_program().unwrap_or_else(|err| {
+            println!("syntax error: {:#?}", err);
+            AstNode::Nil
+        })
     }
 
     fn parse_program(&mut self) -> Result {
-        let mut exprs = vec![];
+        let mut stmts = vec![];
 
         while let Some(t) = self.lexer.peek() {
+            //FIXME: not sure how to remove repetition
             match t.kind {
-                TokenKind::CloseCurlyBrace => break,
+                TokenKind::Semicolon => break,
+                TokenKind::If => {
+                    self.lexer.next();
+                    stmts.push(self.parse_if(t)?)
+                }
+                TokenKind::While => {
+                    self.lexer.next();
+                    stmts.push(self.parse_while(t)?)
+                }
+                TokenKind::Fn => {
+                    self.lexer.next();
+                    stmts.push(self.parse_fn(t)?)
+                }
+                TokenKind::Ret => {
+                    self.lexer.next();
+                    stmts.push(AstNode::RetStmt(Box::new(self.parse_expression()?)))
+                }
                 _ => {
-                    exprs.push(self.parse_expression()?);
-                    self.lexer
-                        .expect(&TokenKind::Semicolon)
-                        .ok_or(Error::MissingSemicolon(t))?;
+                    stmts.push(self.parse_expression()?);
+                    if self.lexer.expect(&TokenKind::Comma).is_none() {
+                        break;
+                    }
                 }
             }
         }
 
-        Ok(AstNode::Program(exprs))
-    }
-
-    fn parse_expression(&mut self) -> Result {
-        if let Some(t) = self.lexer.expect(&TokenKind::Let) {
-            self.parse_assignment(t)
-        } else if let Some(t) = self.lexer.expect(&TokenKind::If) {
-            self.parse_if(t)
-        } else {
-            self.parse_disjunction()
-        }
-    }
-
-    fn parse_assignment(&mut self, t: Token) -> Result {
-        if let Some(id) = self.lexer.expect_identifier() {
-            if self.lexer.expect(&TokenKind::Equal).is_some() {
-                Ok(AstNode::Assignment {
-                    identifier: id,
-                    operand: Box::new(self.parse_expression()?),
-                })
-            } else {
-                Err(Error::AssignmentMissingEqual(t))
-            }
-        } else {
-            Err(Error::AssignmentMissingIdentifier(t))
-        }
+        Ok(AstNode::Program(stmts))
     }
 
     fn parse_if(&mut self, t: Token) -> Result {
-        let check = self.parse_expression()?;
-        if self.lexer.expect(&TokenKind::Then).is_some() {
-            let then = self.parse_expression()?;
-            if self.lexer.expect(&TokenKind::Else).is_some() {
-                let otherwise = self.parse_expression()?;
-                Ok(AstNode::IfExpr {
-                    check: Box::new(check),
-                    then: Box::new(then),
-                    otherwise: Some(Box::new(otherwise)),
-                })
-            } else {
-                Ok(AstNode::IfExpr {
-                    check: Box::new(check),
-                    then: Box::new(then),
-                    otherwise: None,
-                })
-            }
-        } else {
-            Err(Error::IfMissingThenClause(t))
+        let condition = self.parse_expression()?;
+
+        if self.lexer.expect(&TokenKind::Colon).is_none() {
+            return Err(Error::MissingColon(t));
         }
+
+        let then_body = self.parse_program()?;
+
+        let else_body = if self.lexer.expect(&TokenKind::Else).is_some() {
+            Some(self.parse_program()?)
+        } else {
+            None
+        };
+
+        if self.lexer.expect(&TokenKind::Semicolon).is_none() {
+            return Err(Error::MissingSemicolon(t));
+        }
+
+        Ok(AstNode::IfStmt {
+            condition: Box::new(condition),
+            then_body: Box::new(then_body),
+            else_body: else_body.map(Box::new),
+        })
+    }
+
+    fn parse_while(&mut self, t: Token) -> Result {
+        let condition = self.parse_expression()?;
+
+        if self.lexer.expect(&TokenKind::Colon).is_none() {
+            return Err(Error::MissingColon(t))
+        }
+
+        let body = self.parse_program()?;
+
+        if self.lexer.expect(&TokenKind::Semicolon).is_none() {
+            return Err(Error::MissingSemicolon(t));
+        }
+
+        Ok(AstNode::WhileStmt {
+            condition: Box::new(condition),
+            body: Box::new(body),
+        })
+    }
+
+    fn parse_fn(&mut self, t: Token) -> Result {
+        let identifier = self
+            .lexer
+            .expect_identifier()
+            .ok_or(Error::MissingIdentifier(t.clone()))?;
+
+        if self.lexer.expect(&TokenKind::OpenParen).is_none() {
+            return Err(Error::MissingParen(t));
+        }
+
+        let mut params = vec![];
+        if let Some(p) = self.lexer.expect_identifier() {
+            params.push(p);
+
+            while self.lexer.expect(&TokenKind::Comma).is_some() {
+                params.push(self.lexer.expect_identifier().unwrap());
+            }
+        }
+
+        if self.lexer.expect(&TokenKind::CloseParen).is_none() {
+            return Err(Error::MissingParen(t));
+        }
+
+        if self.lexer.expect(&TokenKind::Colon).is_none() {
+            return Err(Error::MissingColon(t));
+        }
+
+        let body = self.parse_program()?;
+
+        if self.lexer.expect(&TokenKind::Semicolon).is_none() {
+            return Err(Error::MissingSemicolon(t));
+        }
+
+        Ok(AstNode::FnStmt {
+            identifier,
+            params,
+            body: Box::new(body),
+        })
+    }
+
+    fn parse_expression(&mut self) -> Result {
+        self.parse_disjunction()
     }
 
     fn parse_disjunction(&mut self) -> Result {
@@ -215,7 +290,7 @@ impl Parser<'source> {
     }
 
     fn parse_comparison(&mut self) -> Result {
-        let lhs = self.parse_addition()?;
+        let lhs = self.parse_modulo()?;
 
         if let Some(t) = self.lexer.expect_any(&[
             TokenKind::Greater,
@@ -226,11 +301,25 @@ impl Parser<'source> {
             return Ok(AstNode::BinaryExpr {
                 operator: (&t.kind).into(),
                 lhs: Box::new(lhs),
-                rhs: Box::new(self.parse_addition()?),
+                rhs: Box::new(self.parse_modulo()?),
             });
         }
 
         Ok(lhs)
+    }
+
+    fn parse_modulo(&mut self) -> Result {
+        let mut acc = self.parse_addition()?;
+
+        while let Some(t) = self.lexer.expect(&TokenKind::Percent) {
+            acc = AstNode::BinaryExpr {
+                operator: (&t.kind).into(),
+                lhs: Box::new(acc),
+                rhs: Box::new(self.parse_addition()?),
+            }
+        }
+
+        Ok(acc)
     }
 
     fn parse_addition(&mut self) -> Result {
@@ -274,34 +363,57 @@ impl Parser<'source> {
 
     fn parse_primary(&mut self) -> Result {
         if let Some(t) = self.lexer.next() {
-            match t.kind {
+            match t.kind.clone() {
                 TokenKind::Number(n) => Ok(AstNode::Number(n)),
                 TokenKind::Boolean(b) => Ok(AstNode::Boolean(b)),
                 TokenKind::StringLiteral(s) => Ok(AstNode::StringLiteral(s)),
-                TokenKind::Identifier(id) => Ok(AstNode::Identifier(id)),
+                TokenKind::Identifier(identifier) => self.parse_identifier(identifier, t),
                 TokenKind::Nil => Ok(AstNode::Nil),
-                TokenKind::OpenParen => {
-                    let expr = self.parse_expression()?;
-
-                    if self.lexer.expect(&TokenKind::CloseParen).is_some() {
-                        Ok(AstNode::Grouping(Box::new(expr)))
-                    } else {
-                        Err(Error::UnclosedGrouping(t))
-                    }
-                }
-                TokenKind::OpenCurlyBrace => {
-                    let program = self.parse_program()?;
-
-                    if self.lexer.expect(&TokenKind::CloseCurlyBrace).is_some() {
-                        Ok(AstNode::Block(Box::new(program)))
-                    } else {
-                        Err(Error::UnclosedBlock(t))
-                    }
-                }
+                TokenKind::OpenParen => self.parse_grouping(t),
                 _ => Err(Error::UnexpectedToken(t)),
             }
         } else {
             Err(Error::UnexpectedEof)
+        }
+    }
+
+    fn parse_identifier(&mut self, identifier: String, t: Token) -> Result {
+        if self.lexer.expect(&TokenKind::OpenParen).is_some() {
+            // fn call
+            let mut args = vec![];
+
+            if let Ok(arg) = self.parse_expression() {
+                args.push(arg);
+
+                while self.lexer.expect(&TokenKind::Comma).is_some() {
+                    args.push(self.parse_expression()?);
+                }
+            }
+
+            if self.lexer.expect(&TokenKind::CloseParen).is_none() {
+                return Err(Error::FnCallMissingCloseParen(t));
+            }
+
+            Ok(AstNode::FnCall { identifier, args })
+        } else if self.lexer.expect(&TokenKind::Equal).is_some() {
+            // assignment
+            Ok(AstNode::Assignment {
+                identifier,
+                operand: Box::new(self.parse_expression()?),
+            })
+        } else {
+            // variable access
+            Ok(AstNode::Identifier(identifier))
+        }
+    }
+
+    fn parse_grouping(&mut self, t: Token) -> Result {
+        let expr = self.parse_expression()?;
+
+        if self.lexer.expect(&TokenKind::CloseParen).is_some() {
+            Ok(AstNode::Grouping(Box::new(expr)))
+        } else {
+            Err(Error::UnclosedGrouping(t))
         }
     }
 }
